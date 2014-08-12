48d2a4c9a14cc276df39a8e0903a40f307bbe727
liujigang <liujigangvip@qq.com>
2014-08-12 17:34:03 +0800

    init2

--- sample/src/main/java/com/huilan/library_videoplay/sample/MainActivity.java
+++ sample/src/main/java/com/huilan/library_videoplay/sample/MainActivity.java
@@ -1,18 +1,20 @@
 package com.huilan.library_videoplay.sample;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.huilan.library_videoplay.AdvancedPlayer;
 import com.huilan.library_videoplay.VideoViewPlay;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
-    private Button play;
+    private Button videoPlay;
+    private Button advancedplayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +24,13 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     }
 
     private void intId() {
-        play = (Button) findViewById(R.id.btn_videoViewPlay);
-        play = (Button) findViewById(R.id.btn_videoViewPlay);
-        play = (Button) findViewById(R.id.btn_videoViewPlay);
-        play = (Button) findViewById(R.id.btn_videoViewPlay);
-        play = (Button) findViewById(R.id.btn_videoViewPlay);
-        play.setOnClickListener(this);
+        videoPlay = (Button) findViewById(R.id.btn_videoViewPlay);
+        advancedplayer = (Button) findViewById(R.id.btn_advancedplayer);
+        videoPlay = (Button) findViewById(R.id.btn_videoViewPlay);
+        videoPlay = (Button) findViewById(R.id.btn_videoViewPlay);
+        videoPlay = (Button) findViewById(R.id.btn_videoViewPlay);
+        videoPlay.setOnClickListener(this);
+        advancedplayer.setOnClickListener(this);
     }
 
 
@@ -48,11 +51,17 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-       switch (v.getId()){
-           case R.id.btn_videoViewPlay:
-               Intent intent = new Intent(getApplication(), VideoViewPlay.class);
-               startActivity(intent);
-               break;
-       }
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.btn_videoViewPlay:
+                intent = new Intent(getApplication(), VideoViewPlay.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_advancedplayer:
+                intent = new Intent(getApplication(), AdvancedPlayer.class);
+                intent.putExtra("path","http://202.108.17.115/v.cctv.com/flash/mp4video32/TMS/2014/02/12/b32eaf65cc07489aabc748a37bfd4b4b_h264418000nero_aac32.mp4");
+                startActivity(intent);
+                break;
+        }
     }
 }
